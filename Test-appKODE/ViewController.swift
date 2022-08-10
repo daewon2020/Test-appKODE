cdbe4d8b12ba5ca933b8527c27e2987454c5eec2
Kostya Andreev <daewon@yandex.ru>
2022-08-10 18:27:32 +0300

    Initial Commit

---
+++ Test-appKODE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-appKODE
+//
+//  Created by Константин Андреев on 10.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
